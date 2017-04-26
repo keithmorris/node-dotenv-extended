07a13e366c4a02aec0db8639c8548b63a80db738
Keith Morris <standupbass@gmail.com>
2017-04-26 14:17:43 -0400

    Add cli binary

---
+++ src/bin/parse-command.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Keith Morris on 4/26/17.
+ */
+const dotEnvFlagRegex = /^--(.+)=(.+)/;
+
+/**
+ * First parses config variables for dotenv-extended then selects the next item as the command and everything after that
+ * are considered arguments for the command
+ *
+ * @param args
+ * @returns {[Object,String,Array]}
+ */
+export const parseCommand = (args) => {
+    const config = {};
+    let command = null;
+    let commandArgs = [];
+    for (let i = 0; i < args.length; i++) {
+        const match = dotEnvFlagRegex.exec(args[i]);
+        if (match) {
+            config[match[1]] = match[2];
+        } else {
+            // No more env setters, the rest of the line must be the command and args
+            command = args[i];
+            commandArgs = args.slice(i + 1);
+            break;
+        }
+    }
+    return [config, command, commandArgs];
+};
